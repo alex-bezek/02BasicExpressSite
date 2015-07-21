dfafb78ace96cc5812ce73b9dbf67a4218bbbfc6
Alex Bezek <alex.liam.bezek@gmail.com>
2015-07-20 21:22:23 -0500

    Added routes and jade files for about and contact. Added some styling to jumbotron and footer

--- routes/about.js
+++ routes/about.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+//the '/' here is because its in its own route file. If it was in index.js it would be removed
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('About', { title: 'About' });
 });
 
 module.exports = router;
